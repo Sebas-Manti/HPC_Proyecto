643c5339c343de0c14676d3aa0ba6201e546b672
Sebas-Manti <sebastian.m_02@hotmail.com>
2026-05-28 01:38:07 -0500

    feat: implement SPH with spatial hash

--- src/python/sph_hashed.py
+++ src/python/sph_hashed.py
@@ -1,3 +1,8 @@
+import numpy as np
+from collections import defaultdict
+from src.python.kernels import W, grad_W, lap_W
+
+
 def build_hash_table(positions, h):
     """
     Construye la tabla hash espacial
@@ -9,7 +14,14 @@ def build_hash_table(positions, h):
     Returns (dict):
     Devuelve una tabla
     """
-    pass
+    cell_size = 2*h
+    hash_table = defaultdict(list)
+    for i in range(len(positions)):
+        cx = np.floor(positions[i,0] / cell_size)
+        cy = np.floor(positions[i,1] / cell_size)
+        key = (cx, cy)
+        hash_table[key].append(i)
+    return hash_table
 
 def query_neighbors(i, positions, hash_table, h):
     """ 
@@ -24,23 +36,41 @@ def query_neighbors(i, positions, hash_table, h):
     Returns (np.ndarray, shape (M,)):
     Los vecinos de la partícula i
     """
-    pass
+    cell_size = 2*h
+    cx = np.floor(positions[i,0] / cell_size)
+    cy = np.floor(positions[i,1] / cell_size)
+    neighbors = []
+    for dx in {-1, 0, 1}:
+        for dy in {-1, 0, 1}:
+            key = (cx+dx, cy+dy)
+            for j in hash_table[key]:
+                if np.linalg.norm(positions[i] - positions[j]) <= 2*h:
+                    neighbors.append(j)
+    return np.array(neighbors)
 
-def compute_density(positions, masses, h):
+def compute_density(positions, masses, hash_table, h):
     """
     Función que calcula la densidad de una partícula con relación a sus vecinos.
 
     Args: 
         positions (np.ndarray, shape (N,2)): Posición de la partícula
         masses (float): Masa de la partícula
+        hash_table (dict): tabla hash espacial
         h (float): Radio de interacción
     
-    Returns (float):
+    Returns (np.ndarray, shape (N,)):
     Devuelve el valor de las densidades
     """
-    pass
+    rho = np.zeros(len(positions))
+    for i in range(len(positions)):
+        for j in query_neighbors(i, positions, hash_table, h):
+            d = positions[i]-positions[j]
+            r = np.linalg.norm(d)
+            rho[i] = masses*W(r,h)+rho[i]
 
-def compute_forces(positions, velocities, densities, masses, h, mu, k, rho0):
+    return rho
+
+def compute_forces(positions, velocities, densities, masses, h, mu, k, rho0, hash_table):
     """
     Función que calcula las fuerzas con relación a sus vecinos.
 
@@ -53,11 +83,31 @@ def compute_forces(positions, velocities, densities, masses, h, mu, k, rho0):
         mu (float): coeficiente de viscocidad
         k (float): coeficiente de rigidez
         rho0 (float): Es la densidad de reposo del fluido
+        hash_table (dict): tabla hash espacial
+
     
-    Returns (np.ndarray):
+    Returns (np.ndarray, shape (N,)):
     Calcula las fuerzas
     """
-    pass
+    v = velocities
+    rho = densities
+    g = np.array([0, -9.8])
+    F = np.zeros((len(positions), 2))
+    for i in range(len(positions)):
+        for j in query_neighbors(i, positions, hash_table, h):
+            if i == j:
+                continue
+            else:
+                d = positions[i]-positions[j]
+                r = np.linalg.norm(d)
+                if 0 < r < 2 * h:
+                    p_i = k * (rho[i] - rho0)
+                    p_j = k * (rho[j] - rho0)
+                    F_press = -masses * (p_i + p_j) / (2 * rho[j]) * grad_W(d, h)
+                    F_visc = mu * masses * (v[j] - v[i]) / rho[j] * lap_W(r, h)
+                    F[i] = F[i] + F_press + F_visc
+        F[i] = F[i] + rho[i] * g
+    return F
 
 def step(positions, velocities, masses, h, dt, mu, k, rho0):
     """
@@ -73,7 +123,24 @@ def step(positions, velocities, masses, h, dt, mu, k, rho0):
         k (float): Coeficiente de rigidez
         rho0 (float): Es la densidad de reposo del fluido
     
-    Returns (float):
+    Returns (np.ndarray, np.ndarray):
     Calcula cada paso
     """
-    pass
+    hash_table = build_hash_table(positions, h)
+    rho = compute_density(positions,masses, hash_table, h)
+    F = compute_forces(positions, velocities, rho, masses, h, mu, k, rho0, hash_table)
+
+    for i in range(len(positions)):
+        velocities[i] = velocities[i] + dt * F[i]/rho[i]
+        positions[i] = positions[i] + dt * velocities[i]
+
+    for i in range(len(positions)):
+        if positions[i,0] < 0 or positions[i,0]>1:
+            positions[i,0] = np.clip(positions[i,0],0,1)
+            velocities[i,0] = -velocities[i,0]
+
+        elif positions[i,1] < 0 or positions[i,1]>1:
+            positions[i,1] = np.clip(positions[i,1],0,1)
+            velocities[i,1] = -velocities[i,1]
+
+    return positions, velocities
