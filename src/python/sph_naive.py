c5afbab677fd59de1e5391508f15421f0b9696e7
Sebas-Manti <sebastian.m_02@hotmail.com>
2026-05-28 00:16:43 -0500

    feat: implement sph_naive compute_density, compute_forces, step

--- src/python/sph_naive.py
+++ src/python/sph_naive.py
@@ -1,3 +1,6 @@
+import numpy as np
+from src.python.kernels import W, grad_W, lap_W
+
 def compute_density(positions, masses, h):
     """
     Función que calcula la densidad de una partícula.
@@ -7,10 +10,19 @@ def compute_density(positions, masses, h):
         masses (float): Masa de la partícula
         h (float): Radio de interacción
     
-    Returns (float):
+    Returns (np.ndarray, shape (N,)):
     Devuelve el valor de las densidades
     """
-    pass
+    rho = np.zeros(len(positions))
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            d = positions[i]-positions[j]
+            r = np.linalg.norm(d)
+            rho[i] = masses*W(r,h)+rho[i]
+
+    return rho
+
+    
 def compute_forces(positions, velocities, densities, masses, h, mu, k, rho0):
     """
     Función que calcula las fuerzas.
@@ -25,10 +37,29 @@ def compute_forces(positions, velocities, densities, masses, h, mu, k, rho0):
         k (float): coeficiente de rigidez
         rho0 (float): Es la densidad de reposo del fluido
     
-    Returns (np.ndarray):
+    Returns (np.ndarray, shape (N,2)):
     Calcula las fuerzas
     """
-    pass
+    v = velocities
+    rho = densities
+    g = np.array([0, -9.8])
+    F = np.zeros((len(positions), 2))
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            if i == j:
+                continue
+            else:
+                d = positions[i]-positions[j]
+                r = np.linalg.norm(d)
+                if 0 < r < 2 * h:
+                    p_i = k * (rho[i] - rho0)
+                    p_j = k * (rho[j] - rho0)
+                    F_press = -masses * (p_i + p_j) / (2 * rho[j]) * grad_W(d, h)
+                    F_visc = mu * masses * (v[j] - v[i]) / rho[j] * lap_W(r, h)
+                    F[i] = F[i] + F_press + F_visc
+        F[i] = F[i] + rho[i] * g
+    return F
+
 
 def step(positions, velocities, masses, h, dt, mu, k, rho0):
     """
@@ -44,8 +75,27 @@ def step(positions, velocities, masses, h, dt, mu, k, rho0):
         k (float): Coeficiente de rigidez
         rho0 (float): Es la densidad de reposo del fluido
     
-    Returns (float):
+    Returns (np.ndarray, np.ndarray):
     Calcula cada paso
     """
-    pass
+    rho = compute_density(positions,masses,h)
+    F = compute_forces(positions, velocities, rho, masses, h, mu, k, rho0)
+
+    for i in range(len(positions)):
+        velocities[i] = velocities[i] * dt + F[i]/rho[i]
+        positions[i] = positions[i] + dt * velocities[i]
+
+    for i in range(len(positions)):
+        if positions[i,0] < 0 or positions[i,0]>1:
+            positions[i,0] = np.clip(positions[i,0],0,1)
+            velocities[i,0] = -velocities[i,0]
+
+        elif positions[i,1] < 0 or positions[i,1]>1:
+            positions[i,1] = np.clip(positions[i,1],0,1)
+            velocities[i,1] = -velocities[i,1]
+
+    return positions, velocities
+
+
+
     
